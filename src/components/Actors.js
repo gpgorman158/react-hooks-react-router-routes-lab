6635c23e7a8da4b08870bfde9e3e4b0879cefe49
Gerald Gorman <gpgorman158@gmail.com>
2022-03-08 00:14:11 -0500

    commit tree 593d9ae615479df3649d31cc835434212819841d

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Actors Page</h1>
+      {actors.map(actor =>
+        {
+          return (
+          <div key={actor.name}>{actor.name}
+            <ul>
+            {actor.movies.map(movie =>
+              <li key={movie}>{movie}</li>)}
+            </ul>
+          
+          </div>
+          
+        )
+        
+        }
+        )
+        }
+    </>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <>
+     <h1>Movies Page</h1>
+     
+      {movies.map(movie => { return (
+      <div key={movie.title}>
+        {`${movie.title} - Movie Duration: ${movie.time}`}
+        <ul>
+          {movie.genres.map(genre => {
+             return <li key={genre}>{genre}</li>
+            })
+            }
+        </ul>
+      </div>
+      )}
+      )}
+
+    </>
+  )
 }
 
 export default Movies;
